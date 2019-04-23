772b99c14b8ab1a1cf0cf25a3c614f24a64b66ac
Tereza Vaňková <vankova.terezaa@gmail.com>
2019-04-23 19:52:47 +0200

    fixroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
 
   resources :apps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/index' => 'main#index'
 
-
+  root to: redirect('/apps')
 end
